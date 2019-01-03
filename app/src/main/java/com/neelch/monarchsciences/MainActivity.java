07741609fda42453a1be1d92dba3c9f2651887c8
neelc17 <neelch1154@gmail.com>
2019-01-03 14:18:27 -0800

    basic webpage loading function added

--- app/src/main/java/com/neelch/monarchsciences/MainActivity.java
+++ app/src/main/java/com/neelch/monarchsciences/MainActivity.java
@@ -2,12 +2,19 @@ package com.neelch.monarchsciences;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.monarchsciences.com/");
     }
 }
