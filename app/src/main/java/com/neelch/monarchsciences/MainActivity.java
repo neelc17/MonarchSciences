28fae601312688c5bea3652465cb0eb975548043
neelc17 <neelch1154@gmail.com>
2019-01-03 14:45:14 -0800

    added basic clearance unlock functionality

--- app/src/main/java/com/neelch/monarchsciences/MainActivity.java
+++ app/src/main/java/com/neelch/monarchsciences/MainActivity.java
@@ -2,11 +2,15 @@ package com.neelch.monarchsciences;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private Button amber;
+    private Button crimson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webView1);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://www.monarchsciences.com/");
+
+        amber = (Button) findViewById(R.id.amber);
+        amber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl("javascript:ConfidentialInfo.unlockAmber()");
+            }
+        });
+
+        crimson = (Button) findViewById(R.id.crimson);
+        crimson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    webView.loadUrl("javascript:ConfidentialInfo.unlockCrimson()");
+            }
+        });
     }
 }
